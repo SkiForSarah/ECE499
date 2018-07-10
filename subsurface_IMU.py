3872de53eae5074e41878ae5dc21a26d417e8a01
haakonsullivan <haakonsullivan@gmail.com>
2018-07-10 13:36:18 -0700

    Update subsurface_IMU.py

--- subsurface_IMU.py
+++ subsurface_IMU.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 from sense_hat import SenseHat # import sense hat library functions into this program
 sense = SenseHat() # define SenseHat() function as sense
 sense.clear() # call clear on SenseHat to clear all measurements
@@ -18,25 +19,25 @@ while guardian<2:
 		# Orientation:
 		o = sense.get_orientation() # define call for orientation function of sense hat as of
 		# o is located in sense, but also has different parameters attached to it, calling it o tidies up the code		
-		pitch = o["pitch"] # read pitch parameter of the orientation function, o
-		roll = o["roll"] # read roll parameter of the orientation function, o
-		yaw = o["yaw"] # read yaw parameter of the orientation function, o
+		sense_data_pitch[k] = o["pitch"] # read pitch parameter of the orientation function, o
+		sense_data_roll[k]= o["roll"] # read roll parameter of the orientation function, o
+		sense_data_yaw[k]= o["yaw"] # read yaw parameter of the orientation function, o
 	
 		# Acceleration:
 		acceleration = sense.get_accelerometer_raw()
-		x = acceleration['x']
-		y = acceleration['y']
-		z = acceleration['z']
+		sense_data_accx[k] = acceleration['x']
+		sense_data_accy[k] = acceleration['y']
+		sense_data_accz[k] = acceleration['z']
 		
-		sense_data_pitch[k]=pitch
-		sense_data_roll[k]=roll
-		sense_data_yaw[k]=yaw
+		Now=datetime.datetime.utcnow()
+		year[k]=Now.year
+		month[k]=Now.month
+		day[k]=Now.day
+		hour[h]=Now.hour
+		minute[k]=Now.minute
+		second[k]=Now.second
+		usecond[k]=Now.microsecond
 		
-		sense_data_accx[k]=x
-		sense_data_accy[k]=y
-		sense_data_accz[k]=z
-		
-	
 	###DSP CODE HERE-> BELOW IS A PLACEHOLDER###
 	filtered_pitch=sense_data_pitch
 	filtered_roll=sense_data_roll
@@ -45,15 +46,13 @@ while guardian<2:
 	filtered_y=sense_data_accy
 	filtered_z=sense_data_accz
 	
-	
-	
 	myFile = open('subsurface_sensor_data.csv', 'a')
 	with myFile:
 		
 		writer = csv.writer(myFile)
 					
-		for k in range(len(filtered_pitch)):
-			senseData = [[filtered_pitch[k], filtered_roll[k], filtered_yaw[k], filtered_x[k], filtered_y[k], filtered_z[k]]]	
+		for k in range(len(filtered_pitch)):#all are the same length, so it shouldn't matter which length is used
+			senseData = [[filtered_pitch[k], filtered_roll[k], filtered_yaw[k], filtered_x[k], filtered_y[k], filtered_z[k], year[k], month[k], day[k], hour[k], minute[k], second[k], usecond[k]]]	
 			writer.writerows(senseData)
 			
 		myFile.close()
